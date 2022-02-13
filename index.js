70c818ad90c1c9e5940741c41a9f9fc8cf14999a
Alex Bajan <alex.i.bajan@gmail.com>
2022-02-13 20:11:35 +0200

    second part

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const express = require('express');
+const cors = require('cors');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const redis = require('redis');
+let RedisStore = require('connect-redis')(session);
+
 const {
   MONGO_USER,
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
+  SESSION_SECRET,
+  REDIS_URL,
+  REDIS_PORT,
 } = require('./config/config');
 
+let redisClient = redis.createClient({
+  host: REDIS_URL,
+  port: REDIS_PORT,
+});
+
+const postRouter = require('./routes/postRoutes');
+const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // db connection
@@ -21,14 +36,47 @@ const connectWithRetry = () => {
     .catch(err => {
       console.log(err), setTimeout(connectWithRetry, 5000);
     });
-
-  // test route
-  app.get('/', (req, res) => {
-    res.send('<h2>Hi there</h2>');
-  });
 };
-
 connectWithRetry();
+
+//
+app.enable('trust proxy');
+
+// NOT WORKING!!! Error: Cannot find module 'cors'
+const corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
+    cookie: {
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 30000,
+    },
+  })
+);
+
+// parse json middleware
+app.use(express.json());
+
+// test route
+app.get('/api/v1', (req, res) => {
+  res.send('<h2>Hi there</h2>');
+  console.log('test');
+});
+
+// mMOUNT ROUTERS
+// localhost:3000/api/v1/posts
+app.use('/api/v1/posts', postRouter);
+// localhost:3000/api/v1/users
+app.use('/api/v1/users', userRouter);
 
 // open a port and listen for connection
 const port = process.env.PORT || 3000;
---
+++ routes/postRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const postController = require('../controllers/postController');
+const protect = require('../middleware/authMiddleware.js');
+
+const router = express.Router();
+
+// localhost:3000/
+router
+  .route('/')
+  .get(protect, postController.getAllPosts)
+  .post(protect, postController.createPost);
+
+// localhost:3000/:id
+router
+  .route('/:id')
+  .get(protect, postController.getOnePost)
+  .patch(protect, postController.updatePost)
+  .delete(protect, postController.deletePost);
+
+module.exports = router;
